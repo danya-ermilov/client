1131289ed1db689b7f81aea73772697450df3d53
danya-ermilov <141646057+danya-ermilov@users.noreply.github.com>
2023-08-24 19:52:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,43 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+
+const HTTP_PORT = process.env.HTTP_PORT || 80;
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Путь к вашим SSL-сертификатам
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/botoginvest.com/prevkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/botoginvest.com/cert.pem"),
+};
+
+// Создание HTTPS сервера
+const httpsServer = https.createServer(options, app);
+
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`HTTPS server is running on port ${HTTPS_PORT}`);
+});
+
+// HTTP сервер для перенаправления на HTTPS
+const httpServer = express();
+httpServer.get("*", (req, res) => {
+  res.redirect(`https://${req.headers.host}${req.url}`);
+});
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`HTTP server is running on port ${HTTP_PORT}`);
+});
+
+/*
 const express = require('express');
 const path = require("path");
 
@@ -12,3 +52,4 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT);
+*/
