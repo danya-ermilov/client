ea566f7b973c8b18d1ff3acceb541a57e427e6ff
danya-ermilov <141646057+danya-ermilov@users.noreply.github.com>
2023-08-26 20:05:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+/*
 const express = require("express");
 const https = require("https");
 const fs = require("fs");
@@ -37,7 +37,7 @@ httpServer.get("*", (req, res) => {
 httpServer.listen(HTTP_PORT, () => {
   console.log(`HTTP server is running on port ${HTTP_PORT}`);
 });
-/*
+*/
 
 const express = require('express');
 const path = require("path");
@@ -53,4 +53,3 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT);
-*/
